dbe1e4b6817d5f2680817eed0e9923551520b130
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-08-10 11:12:05 +0900

    4-9

--- 4/DameStudent.java
+++ 4/DameStudent.java
@@ -1,6 +1,6 @@
-public class DameStudent extends Student4{
+/*public class DameStudent extends Student4{
   DameStudent(String name, int stuNo){
     super(name, stuNo);
   }
-}
+}*/
 // コンパイルするとエラーになる
